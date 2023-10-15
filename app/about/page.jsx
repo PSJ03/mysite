977224a34574bd73c9e975f58d0b6f7304cf411f
PSJ03 <craftnarusasu@gmail.com>
2023-10-16 04:18:17 +0900

    5th

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -11,6 +11,15 @@ export default function AboutPage() {
           전공 과목을 배우는 것이 여러모로 어렵지만 열심히 이해하고 배우려고
           노력하고 있습니다.
         </div>
+        <div className="m-4 p-4 rounded-lg bg-slate-300 border border-slate-400">
+          <h1 className="font0-bold text-2xl underline text-indigo-400">
+            Project
+          </h1>
+          팀 프로젝트(김승언, 박서진, 유혁준, 신준혁) - 진행 중, 혹은 끝난
+          게임의 전적을 검색하는 웹사이트를 제작 계획입니다.
+          <br />
+          진행 단계를 확인하고 싶으시다면 홈에서 Projects 탭을 클릭해주세요.
+        </div>
       </div>
     </>
   )
