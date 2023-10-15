30e38ba53e3b15f83e0ab019047d8f3ec50c261f
PSJ03 <craftnarusasu@gmail.com>
2023-10-16 04:20:48 +0900

    6th

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
     <>
       <div>
         <div className="m-4 p-4 rounded-lg bg-slate-300 border border-slate-400">
-          <h1 className="font0-bold text-2xl underline text-indigo-400">
+          <h1 className="font0-bold text-2xl underline text-indigo-400 font-bold">
             저에 대해서
           </h1>
           저는 중부대학교에서 정보보호학과를 전공하고 박서진이라고 합니다.
@@ -12,12 +12,12 @@ export default function AboutPage() {
           노력하고 있습니다.
         </div>
         <div className="m-4 p-4 rounded-lg bg-slate-300 border border-slate-400">
-          <h1 className="font0-bold text-2xl underline text-indigo-400">
-            Project
+          <h1 className="font0-bold text-2xl underline text-purple-500 font-bold">
+            현재 프로젝트
           </h1>
-          팀 프로젝트(김승언, 박서진, 유혁준, 신준혁) - 진행 중, 혹은 끝난
-          게임의 전적을 검색하는 웹사이트를 제작 계획입니다.
+          팀원 -김승언, 박서진, 유혁준, 신준혁
           <br />
+          진행 중, 혹은 끝난 게임의 전적을 검색하는 웹사이트를 제작 계획입니다.
           진행 단계를 확인하고 싶으시다면 홈에서 Projects 탭을 클릭해주세요.
         </div>
       </div>
