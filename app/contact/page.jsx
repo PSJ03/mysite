f0d4b205fb32a91d5a7c8c0cdb833f5c2c8a28b8
PSJ03 <craftnarusasu@gmail.com>
2023-10-16 10:49:30 +0900

    9th

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,11 +2,11 @@ export default function AboutPage() {
   return (
     <>
       <div className="m-4 p-3 rounded-lg bg-white border border-gray-500 border-opacity-60 text-center">
-        <div className="text-2xl">Phone</div>
+        <div className="text-2xl font-bold">Phone</div>
         010-9502-2091
       </div>
       <div className="m-4 p-3 rounded-lg bg-white border border-gray-500 border-opacity-60 text-center">
-        <div className="text-2xl">Email</div>
+        <div className="text-2xl font-bold">Email</div>
         <p>Goggle : craftnarusasu@gmail.com</p>
         <p>Naver : paksongpak@naver.com</p>
       </div>
